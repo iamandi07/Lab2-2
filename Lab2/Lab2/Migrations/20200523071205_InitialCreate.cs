e8ff76efd6686ad443a264e41099f5d0b14409ce
Szabi <szabiadam@yahoo.com>
2020-05-23 10:32:10 +0300

    aaa

---
+++ Lab2/Lab2/Migrations/20200523071205_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpensesClasses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: true),
+                    Sum = table.Column<double>(nullable: false),
+                    Location = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Currency = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Importance = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpensesClasses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpensesClasses");
+        }
+    }
+}
